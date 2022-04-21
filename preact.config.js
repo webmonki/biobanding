425654cd76652a801645c2f96298295189cf8d8e
Lukas Kaufer <lukas.kaufer@gmail.com>
2022-04-21 11:52:47 +0200

    Add config for cloud deployment

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,9 @@
 import asyncPlugin from 'preact-cli-plugin-async';
 
-export default (config) => {
+export default (config, env, helpers) => {
+    const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
+    plugin.definitions['process.env.PREACT_APP_HOST_URI'] = JSON.stringify(process.env.PREACT_APP_HOST_URI);
     asyncPlugin(config);
 }
+
+
