b335686943acec829719ad8f78db4609a7851f49
philipp_langnau <philipp.langnau@vp-systeme.de>
2022-04-29 16:43:22 +0200

    menu.js and sortIcon.js

---
+++ src/components/table/menu.js
@@ -0,0 +1,57 @@
+import { h, Component } from "preact";
+import style from "./style";
+import Button from "preact-material-components/Button";
+import "preact-material-components/Button/style.css";
+import List from "preact-material-components/List";
+import TextField from "preact-material-components/TextField";
+import "preact-material-components/TextField/style.css";
+
+export default class Menu extends Component {
+  componentWillMount = () => {
+    this.setState({ containerDelete: style.dontDisplay });
+    this.setState({ containerAddSearch: style.searchContainer });
+  };
+
+  render() {
+    let content;
+    if (this.props.showDelete) {
+      content = (
+        <div class={style.deleteContainer}>
+          <Button class={style.menuItem} onClick={this.props.checkDelete}>
+            <List.ItemGraphic class={`${"mdc-theme--primary"}`}>
+              delete
+            </List.ItemGraphic>
+          </Button>
+        </div>
+      );
+    } else {
+      content = (
+        <div class={style.searchContainer}>
+          <TextField
+            autocomplete="off"
+            outlined
+            label="Suche"
+            value={this.state.search}
+            onKeyUp={(e) => {
+              let val = e.target.value;
+              this.setState({ search: val });
+              this.props.search(val);
+            }}
+          />
+          <Button raised onClick={this.props.showDialog}>
+            <i
+              class="material-icons mdc-button__icon mdc-theme-on-primary"
+              aria-hidden="true"
+            >
+              add
+            </i>
+            <span class="mdc-button__label mdc-theme-on-primary">
+              erstellen
+            </span>
+          </Button>
+        </div>
+      );
+    }
+    return <div>{content}</div>;
+  }
+}
