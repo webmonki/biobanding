3ab44981863ec8cdb0e005deeee280ad8b6b1e2c
Lukas Kaufer <lukas.kaufer@gmail.com>
2022-05-10 11:06:04 +0200

    Fix broken testcases

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -2,17 +2,22 @@
 """
 Copyright (c) 2022 - present | VP-Systeme GmbH, Lyrenstr. 13, 44866 Bochum
 """
+from datetime import datetime, timedelta
 
 import json
+import pytest
+import jwt
+
+from api.config import BaseConfig
 
 
 """
    Sample test data
 """
 # Login data
-DUMMY_USERNAME = "admin"
-DUMMY_EMAIL = "admin@example.org"
-DUMMY_PASS = "admin"
+DUMMY_USERNAME = "admin2"
+DUMMY_EMAIL = "admin2@example.org"
+DUMMY_PASS = "admin2"
 # Configuration
 DUMMY_EMAIL_SERVER = "smtp.example-server.org"
 DUMMY_EMAIL_PORT = 100
@@ -38,7 +43,7 @@ def test_user_signup(client):
 
     data = json.loads(response.data.decode())
     assert response.status_code == 200
-    assert "The user was successfully registered" in data["msg"]
+    assert "The user was successfully registered and a confirmation link was send" in data["msg"]
 
 
 def test_user_signup_invalid_data(client):
@@ -59,6 +64,54 @@ def test_user_signup_invalid_data(client):
     data = json.loads(response.data.decode())
     assert response.status_code == 400
     assert "'' is too short" in data["msg"]
+
+
+def test_user_login_unconfirmed(client):
+    """
+       Tests api/users/login API: Email address is not confirmed
+   """
+
+    response = client.post(
+        "api/users/login",
+        data=json.dumps(
+            {
+                "email": DUMMY_EMAIL,
+                "password": DUMMY_PASS
+            }
+        ),
+        content_type="application/json")
+
+    data = json.loads(response.data.decode())
+    assert response.status_code == 403
+    assert "Email address is not confirmed" in data["msg"]
+
+
+def test_user_confirm_signup(client):
+    """
+    Tests /api/users/confirm API: Successful confirmed account. User is Logged in
+    """
+
+    token = jwt.encode({'email': DUMMY_EMAIL, 'exp': datetime.utcnow() + timedelta(hours=1)}, BaseConfig.SECRET_KEY)
+    response = client.post(
+        '/api/users/confirm',
+        headers={"authorization": token},
+        data=json.dumps(
+            {
+                "last_name": DUMMY_USERNAME,
+                "first_name": DUMMY_USERNAME,
+                "birthday": "2015-05-10",
+                "sex_m_0_f_1": 0,
+                "height_father": 175,
+                "height_mother": 164
+            }
+        ),
+        content_type="application/json")
+
+    data = json.loads(response.data.decode())
+    assert response.status_code == 201
+    assert data["token"] != ""
+    assert data["user"] != ""
+    assert "Successful confirmed account. User is Logged in" in data['msg']
 
 
 def test_user_login_correct(client):
@@ -89,17 +142,18 @@ def test_user_login_error(client):
         data=json.dumps(
             {
                 "email": DUMMY_EMAIL,
-                "password": DUMMY_EMAIL
+                "password": DUMMY_PASS + "x"
             }
         ),
         content_type="application/json")
 
     data = json.loads(response.data.decode())
-    assert response.status_code == 400
+    assert response.status_code == 401
     assert "Wrong credentials." in data["msg"]
 
 
 # Todo
+@pytest.mark.skip(reason="Not implemented")
 def test_update_user_by_id(client):
     """
         Tests /api/user/<int:id> API: Successfully updated user data
@@ -107,6 +161,7 @@ def test_update_user_by_id(client):
 
 
 # Todo
+@pytest.mark.skip(reason="Not implemented")
 def test_delete_user_by_id(client):
     """
         Tests /api/user/<int:id> API: Successfully updated user data
@@ -114,6 +169,7 @@ def test_delete_user_by_id(client):
 
 
 # Todo
+@pytest.mark.skip(reason="Not implemented")
 def test_get_user_by_id(client):
     """
     Tests /api/user/<int:id> API: Successfully deleted user
