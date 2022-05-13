3a3e802d8a601e2882c7caccb647e745e621125e
Lukas Kaufer <lukas.kaufer@gmail.com>
2022-05-13 12:46:08 +0200

    Add os env PREACT_APP_DSGVO & PREACT_APP_IMPRESSUM

--- preact.config.js
+++ preact.config.js
@@ -4,6 +4,8 @@ export default (config, env, helpers) => {
     const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
     plugin.definitions['process.env.PREACT_APP_HOST_URI'] = JSON.stringify(process.env.PREACT_APP_HOST_URI);
     plugin.definitions['process.env.PREACT_APP_VERSION'] = JSON.stringify(process.env.PREACT_APP_VERSION);
+    plugin.definitions['process.env.PREACT_APP_IMPRESSUM'] = JSON.stringify(process.env.PREACT_APP_IMPRESSUM);
+    plugin.definitions['process.env.PREACT_APP_DSGVO'] = JSON.stringify(process.env.PREACT_APP_DSGVO);
     asyncPlugin(config);
 }
 
