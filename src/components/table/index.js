4ba19d2f4837f6ad6135ba7e99b17fd0850251e2
philipp_langnau <philipp.langnau@vp-systeme.de>
2022-05-21 13:31:45 +0200

    table layout

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -64,7 +64,7 @@ export default class Table extends Component {
       window.innerWidth || 0
     );
 
-    let index = Math.ceil(vw / 160);
+    let index = Math.ceil(vw / 140);
 
     if (index <= 4) {
       this.setState({ subTableIndex: 4 });
