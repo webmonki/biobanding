f1fa3a5e2ae925258281012249baf0e4b03825f6
Lukas Kaufer <lukas.kaufer@gmail.com>
2022-05-25 11:07:43 +0200

    Change password lenght in db

--- api/models.py
+++ api/models.py
@@ -21,7 +21,7 @@ class Users(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(64), nullable=False)
-    password = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
     date_joined = db.Column(db.DateTime(), default=datetime.utcnow)
     jwt_auth_active = db.Column(db.Boolean())
     is_admin = db.Column(db.Boolean())
